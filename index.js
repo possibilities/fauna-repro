d6e2e49cd1117f43645b4f1c41866a24ef0c6854
Mike Bannister <notimpossiblemike@gmail.com>
2019-12-30 17:43:27 -0500

    Fixed

--- index.js
+++ index.js
@@ -2,34 +2,41 @@ const faunadb = require('faunadb')
 const q = faunadb.query
 
 // Admin secret created at https://app.fauna.com/keys
-const { faunadb_secret: adminSecret } = process.env
+const {
+  faunadb_name: dbName,
+  faunadb_secret: rootSecret
+} = process.env
 
 async function main() {
-  const adminClient = new faunadb.Client({ secret: adminSecret })
+  const rootClient = new faunadb.Client({ secret: rootSecret })
 
   // Delete db if exists
-  await adminClient.query(
-    q.Delete(q.Database('my_db'))
+  await rootClient.query(
+    q.Delete(q.Database(dbName))
   ).catch(function() {})
 
-  await adminClient.query(
-    q.CreateDatabase({ name: 'my_db' })
+  await rootClient.query(
+    q.CreateDatabase({ name: dbName })
   )
 
-  // Get a server secret for created database
-  const { secret: serverSecret } = await adminClient.query(
-    q.CreateKey({
-      role: 'server',
-      database: q.Database('my_db')
-    })
+  const { secret: serverSecret } = await rootClient.query(
+    q.CreateKey({ role: 'server', database: q.Database(dbName) })
+  )
+  const { secret: adminSecret } = await rootClient.query(
+    q.CreateKey({ role: 'admin', database: q.Database(dbName) })
   )
 
-  // New client with server secret
+  const adminClient = new faunadb.Client({ secret: adminSecret })
   const serverClient = new faunadb.Client({ secret: serverSecret })
 
   // Create collections
   await serverClient.query(q.CreateCollection({ name: 'widgets' }))
+    .catch(function(error) { console.info('widgets:', error.message) })
+
+  // Create user collection and index
   await serverClient.query(q.CreateCollection({ name: 'users' }))
+    .catch(function(error) { console.info('users:', error.message) })
+
   await serverClient.query(
     q.CreateIndex({
       name: 'users_by_email',
@@ -38,14 +45,13 @@ async function main() {
       terms: [{ field: ['data', 'email'] }],
       unique: true
     })
-  )
+  ).catch(function(error) { console.info('users_by_email:', error.message) })
 
   // Delete role if exists
   await adminClient.query(
     q.Delete(q.Role('access_content'))
   ).catch(function () {})
 
-  // Why does this fail?
   await adminClient.query(
     q.CreateRole({
       name: 'access_content',

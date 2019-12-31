3e5859b0e453c57502eaf08beef5e02fc068c1d7
Mike Bannister <notimpossiblemike@gmail.com>
2019-12-30 20:39:12 -0500

    Do some work

--- index.js
+++ index.js
@@ -31,6 +31,12 @@ async function main() {
 
   // Create collections
   await serverClient.query(q.CreateCollection({ name: 'widgets' }))
+  await serverClient.query(
+    q.CreateIndex({
+      name: 'all_widgets',
+      source: q.Collection('widgets')
+    })
+  )
 
   // Create user collection and index
   await serverClient.query(q.CreateCollection({ name: 'users' }))
@@ -48,11 +54,60 @@ async function main() {
     q.CreateRole({
       name: 'access_content',
       membership: [{ resource: q.Collection('users') }],
-      privileges: [{
-        resource: q.Collection('widgets'),
-        actions: { read: true }
-      }]
+      privileges: [
+        {
+          resource: q.Index('all_widgets'),
+          actions: { read: true }
+        },
+        {
+          resource: q.Collection('widgets'),
+          actions: { read: true, create: true }
+        }
+      ]
     })
+  )
+
+  await serverClient.query(
+    q.Create(
+      q.Collection('users'),
+      {
+        credentials: { password: 'secret password' },
+        data: { email: 'alice@site.example' }
+      }
+    )
+  )
+
+  const { secret: aliceSecret } =
+    await serverClient.query(
+      q.Login(
+        q.Match(q.Index('users_by_email'), 'alice@site.example'),
+        { password: 'secret password' }
+      )
+    )
+
+  const aliceClient = new faunadb.Client({ secret: aliceSecret })
+
+  await aliceClient.query(
+    q.Create(
+      q.Collection('widgets'),
+      { data: { title: 'foo' } }
+    )
+  )
+
+  await aliceClient.query(
+    q.Create(
+      q.Collection('widgets'),
+      { data: { title: 'bar' } }
+    )
+  )
+
+  console.log(
+    (await aliceClient.query(
+      q.Map(
+        q.Paginate(q.Match(q.Index('all_widgets'))),
+        q.Lambda('widget', q.Get(q.Var('widget')))
+      )
+    )).data.map(doc => doc.data)
   )
 }
 
